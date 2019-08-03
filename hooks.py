6ca8916abc5488b643089a82dbffca2e53d98d36
Osher Lerner <osherler@gmail.com>
2019-08-02 21:23:29 -0400

    brainstorming knotty painting hanging problem

---
+++ hooks.py
@@ -0,0 +1,49 @@
+# knot = [0, 1, 2, 3]
+
+
+# most_efficient = {i: (n, [])}
+def unhook(knot):
+
+
+def conjugate(knot):
+    
+
+
+most_efficient = {
+    1: (1, [[1]]),
+    2: (4, [[1,2,-1,-2]])
+}
+
+# def partitions(n, k):
+#     nums = []
+#     for k_left in range(k-1, 0, 1):
+#         for num in range(1, n - sum(nums) k_left + 1):
+
+
+for n in range(2, 10):
+    for k in range(2,n):
+
+
+
+
+def combine_knots(metaknot, knots):
+
+
+
+
+
+
+# combinations
+
+most_efficient = {
+    1: 1,
+    2: 4
+}
+
+most_efficient_combinations = {
+    1: [1],
+    2: [2, 2]
+} 
+
+n = 3
+for i in range(n):
